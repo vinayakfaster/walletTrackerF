827b2aefb4abe690e13093e570c2c523ee1bdb3e
vinayak shukla <65018895+vinayakfaster@users.noreply.github.com>
2024-04-28 17:17:49 +0530

    Update TransactionMonitor.js

--- components/TransactionMonitor.js
+++ components/TransactionMonitor.js
@@ -33,7 +33,7 @@ const [addressesSymbols, setAddressesSymbols] = useState([]);
 
 
   useEffect(() => {
-    const socket = new WebSocket('https://wallettrackerb.onrender.com:5003');
+    const socket = new WebSocket('https://wallettrackerb.onrender.com');
   
     socket.onopen = () => {
       console.log('WebSocket connected');
